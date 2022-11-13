226e4fb4740a444188553a10e4ab095f9cf0f8d4
Burxon Fozilov <113450585+BurxonCoder@users.noreply.github.com>
2022-11-13 12:29:35 +0500

    oshanjdb

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import '../components/HeroimageStyle.css'
+
 export class Heroimgae extends Component {
   render() {
     return (
       <div className='hero-img'>
          <div className='heading'>
-            <h1>{this.props.headin}</h1>
+            <h1>{this.props.heading}</h1>
             <p>{this.props.text}</p>
-    </div>        
+           </div>        
       </div>
     )
   }
--- src/routes/Pricing.js
+++ src/routes/Pricing.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import Footer from '../components/Footer'
-import Heroimgae from '../components/Heroimgae'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import PricingCards from '../components/Pricing'
+import HeroImage from '../components/HeroImage'
 
 const Pricing = () => {
-  return (
-    <div>
-        <Navbar />
-        <Heroimgae />
-        <Footer />
-    </div>
-  )
+    return (
+        <div>
+            <Navbar />
+            <HeroImage heading='PRICING.' text='Choose your trip.' />
+            <PricingCards />
+            <Footer />
+        </div>
+    )
 }
 
 export default Pricing
